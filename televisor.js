78ed67d375747dd258f7b32be715d582d55f6acc
FacundoRetamar <facundo1retamar@gmail.com>
2023-08-14 10:34:38 -0300

    tarea televisor

---
+++ televisor.js
@@ -0,0 +1,63 @@
+var Televisor = /** @class */ (function () {
+    function Televisor() {
+        this.encendido = false;
+        this.volumen = 50;
+        this.canal = 1;
+    }
+    Televisor.prototype.encender = function () {
+        this.encendido = true;
+        console.log("Televisor encendido.");
+    };
+    Televisor.prototype.apagar = function () {
+        this.encendido = false;
+        console.log("Televisor apagado.");
+    };
+    Televisor.prototype.cambiarCanal = function (nuevoCanal) {
+        if (this.encendido) {
+            if (nuevoCanal >= 1 && nuevoCanal <= 99) {
+                this.canal = nuevoCanal;
+                console.log("Cambiando al canal ".concat(nuevoCanal, "."));
+            }
+            else {
+                console.log("Error: El canal debe estar entre 1 y 99.");
+            }
+        }
+        else {
+            console.log("Error: El televisor está apagado.");
+        }
+    };
+    Televisor.prototype.ajustarVolumen = function (nuevoVolumen) {
+        if (this.encendido) {
+            if (nuevoVolumen >= 0 && nuevoVolumen <= 100) {
+                this.volumen = nuevoVolumen;
+                console.log("Ajustando volumen a ".concat(nuevoVolumen, "."));
+            }
+            else {
+                console.log("Error: El volumen debe estar entre 0 y 100.");
+            }
+        }
+        else {
+            console.log("Error: El televisor está apagado.");
+        }
+    };
+    Televisor.prototype.mostrarInfo = function () {
+        if (this.encendido) {
+            var horaActual = new Date().toLocaleString();
+            console.log("Canal: ".concat(this.canal, ", Volumen: ").concat(this.volumen, ", Hora actual: ").concat(horaActual));
+        }
+        else {
+            console.log("El televisor está apagado.");
+        }
+    };
+    return Televisor;
+}());
+// Ejemplo de uso
+const miTelevisor = new Televisor();
+miTelevisor.encender();
+miTelevisor.cambiarCanal(8);
+miTelevisor.ajustarVolumen(55);
+miTelevisor.mostrarInfo();
+miTelevisor.apagar();
+miTelevisor.cambiarCanal(5);
+miTelevisor.ajustarVolumen(23);
+miTelevisor.cambiarCanal(14);
---
+++ televisor.ts
@@ -0,0 +1,67 @@
+class Televisor {
+    private encendido: boolean;
+    private volumen: number;
+    private canal: number;
+
+    constructor() {
+        this.encendido = false;
+        this.volumen = 50;
+        this.canal = 1;
+    }
+
+    encender(): void {
+        this.encendido = true;
+        console.log("Televisor encendido.");
+    }
+
+    apagar(): void {
+        this.encendido = false;
+        console.log("Televisor apagado.");
+    }
+
+    cambiarCanal(nuevoCanal: number): void {
+        if (this.encendido) {
+            if (nuevoCanal >= 1 && nuevoCanal <= 99) {
+                this.canal = nuevoCanal;
+                console.log(`Cambiando al canal ${nuevoCanal}.`);
+            } else {
+                console.log("Error: El canal debe estar entre 1 y 99.");
+            }
+        } else {
+            console.log("Error: El televisor está apagado.");
+        }
+    }
+
+    ajustarVolumen(nuevoVolumen: number): void {
+        if (this.encendido) {
+            if (nuevoVolumen >= 0 && nuevoVolumen <= 100) {
+                this.volumen = nuevoVolumen;
+                console.log(`Ajustando volumen a ${nuevoVolumen}.`);
+            } else {
+                console.log("Error: El volumen debe estar entre 0 y 100.");
+            }
+        } else {
+            console.log("Error: El televisor está apagado.");
+        }
+    }
+
+    mostrarInfo(): void {
+        if (this.encendido) {
+            const horaActual: string = new Date().toLocaleString();
+            console.log(`Canal: ${this.canal}, Volumen: ${this.volumen}, Hora actual: ${horaActual}`);
+        } else {
+            console.log("El televisor está apagado.");
+        }
+    }
+}
+
+// Ejemplo de uso
+const miTelevisor = new Televisor();
+miTelevisor.encender();
+miTelevisor.cambiarCanal(8);
+miTelevisor.ajustarVolumen(55);
+miTelevisor.mostrarInfo();
+miTelevisor.apagar();
+miTelevisor.cambiarCanal(5);
+miTelevisor.ajustarVolumen(55);
+miTelevisor.cambiarCanal(8);
